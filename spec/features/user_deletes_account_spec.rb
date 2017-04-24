e79e4a7ad65f152595e8827684ee092af06b506f
Greg Rose <gregrose628@gmail.com>
2017-04-24 13:38:09 -0400

    Added user account deletion, made site mobile-ready

---
+++ spec/features/user_deletes_account_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+# As an authenticated user
+# I want to delete my account
+# So that my information is no longer retained by the app
+#
+# Acceptance Criteria:
+#   - I must be signed in
+#   - I must go to the my account page
+#   - I must click and Delete My Account
+#   - If I then click OK, my account is deleted
+
+feature 'user deletes account' do
+  let!(:user) do
+    create(:user)
+  end
+
+  scenario 'successfully delete account' do
+    sign_in user
+    visit root_path
+    click_link 'My Account'
+    click_button 'Delete My Account'
+
+    expect(page).to have_content 'Your account has been deleted.'
+    expect(page).to_not have_content 'Sign Out'
+    expect(User.count).to be 0
+  end
+end
