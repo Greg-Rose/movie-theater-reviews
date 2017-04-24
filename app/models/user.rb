64e6de1788076b3923ae0acf8b09fac9ca9a019b
Greg Rose <gregrose628@gmail.com>
2017-04-24 18:57:23 -0400

    fixed spacing typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,16 @@ class User < ApplicationRecord
 
   # instead of deleting, indicate the user requested a delete & timestamp it
   def soft_delete
-   update_attribute(:deleted_at, Time.current)
+    update_attribute(:deleted_at, Time.current)
   end
 
   # ensure user account is active
   def active_for_authentication?
-   super && !deleted_at
+    super && !deleted_at
   end
 
   # provide a custom message for a deleted account
   def inactive_message
-  	!deleted_at ? super : :deleted_account
+    !deleted_at ? super : :deleted_account
   end
 end
