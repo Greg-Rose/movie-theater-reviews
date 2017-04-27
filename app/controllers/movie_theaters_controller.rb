a59a2625541679030c05e138d39313c0d040317b
Greg Rose <gregrose628@gmail.com>
2017-04-26 22:54:24 -0400

    added movie theater updating

--- app/controllers/movie_theaters_controller.rb
+++ app/controllers/movie_theaters_controller.rb
@@ -1,5 +1,6 @@
 class MovieTheatersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorize_user, only: [:edit, :update]
 
   def index
     @movie_theaters = MovieTheater.all
@@ -27,9 +28,32 @@ class MovieTheatersController < ApplicationController
     end
   end
 
+  def edit
+    @states = State.all
+    @movie_theater = MovieTheater.find(params[:id])
+  end
+
+  def update
+    @movie_theater = MovieTheater.find(params[:id])
+    if @movie_theater.update(movie_theater_params)
+      redirect_to @movie_theater, notice: "Movie Theater Updated!"
+    else
+      @states = State.all
+      flash.now[:alert] = @movie_theater.errors.full_messages
+      render :edit
+    end
+  end
+
   private
 
   def movie_theater_params
     params.require(:movie_theater).permit(:name, :address, :city, :state_id, :zipcode, :website)
+  end
+
+  def authorize_user
+    movie_theater = MovieTheater.find(params[:id])
+    if !user_signed_in? || current_user != movie_theater.user
+      redirect_to movie_theater, alert: "You Can Only Update Theaters You've Added!"
+    end
   end
 end
