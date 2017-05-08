9b578d0dfbc817a97464df2055762dbfa11afa1d
Greg Rose <gregrose628@gmail.com>
2017-05-08 00:19:23 -0400

    added more tests for api v1 votes controller

--- spec/controllers/api/v1/votes_controller_spec.rb
+++ spec/controllers/api/v1/votes_controller_spec.rb
@@ -32,6 +32,39 @@ RSpec.describe Api::V1::VotesController, type: :controller do
       expect(response.header["Location"]).to match /\/api\/v1\/votes\/upvote/
     end
 
+    it "changes vote to upvote" do
+      vote = create(:vote, review: review, user: user, helpful: false)
+
+      sign_in user
+
+      post :upvote, params: { vote: vote.attributes }
+
+      expect(response).to have_http_status(:created)
+      expect(response.header["Location"]).to match /\/api\/v1\/votes\/upvote/
+    end
+
+    it "doesn't create duplicate upvote" do
+      vote = create(:vote, review: review, user: user, helpful: true)
+
+      sign_in user
+
+      post :upvote, params: { vote: vote.attributes }
+
+      expect(response).to have_http_status(:created)
+      expect(response.header["Location"]).to match /\/api\/v1\/votes\/upvote/
+    end
+
+    it "can't upvote your own review" do
+      vote = build(:vote, review: review)
+
+      sign_in review.user
+
+      post :upvote, params: { vote: vote.attributes }
+
+      expect(response).to have_http_status(:created)
+      expect(response.header["Location"]).to match /\/api\/v1\/votes\/upvote/
+    end
+
     it "creates a new downvote" do
       # a Comment object is built but not persisted to the database
       vote = build(:vote, review: review, helpful: false)
@@ -47,6 +80,39 @@ RSpec.describe Api::V1::VotesController, type: :controller do
       # assert that the response has a specific HTTP status
       expect(response).to have_http_status(:created)
       # assert that the response has a header with a specific value
+      expect(response.header["Location"]).to match /\/api\/v1\/votes\/downvote/
+    end
+
+    it "changes vote to downvote" do
+      vote = create(:vote, review: review, user: user, helpful: true)
+
+      sign_in user
+
+      post :downvote, params: { vote: vote.attributes }
+
+      expect(response).to have_http_status(:created)
+      expect(response.header["Location"]).to match /\/api\/v1\/votes\/downvote/
+    end
+
+    it "doesn't create duplicate downvote" do
+      vote = create(:vote, review: review, user: user, helpful: false)
+
+      sign_in user
+
+      post :downvote, params: { vote: vote.attributes }
+
+      expect(response).to have_http_status(:created)
+      expect(response.header["Location"]).to match /\/api\/v1\/votes\/downvote/
+    end
+
+    it "can't downvote your own review" do
+      vote = build(:vote, review: review)
+
+      sign_in review.user
+
+      post :downvote, params: { vote: vote.attributes }
+
+      expect(response).to have_http_status(:created)
       expect(response.header["Location"]).to match /\/api\/v1\/votes\/downvote/
     end
   end
