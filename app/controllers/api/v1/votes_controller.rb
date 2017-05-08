b67db6ac8bf1d6a449d66a0b48043984643488f8
Greg Rose <gregrose628@gmail.com>
2017-05-07 23:10:29 -0400

    stopped ajax voting of ones own reviews

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -1,40 +1,45 @@
 class Api::V1::VotesController < Api::V1::ApiController
   before_action :authenticate_user!
-  before_action :authorize_user
 
   def upvote
     @review = Review.find(vote_params[:review_id])
     @vote = Vote.find_by(user: current_user, review: @review)
-
-    if @vote
-      if @vote.helpful == true
-        render_json("You can change your answer but you can't answer again.")
+    if current_user == @review.user
+      render_json("You can't vote on your own reviews.")
+    else
+      if @vote
+        if @vote.helpful == true
+          render_json("You can change your answer but you can't answer again.")
+        else
+          @vote.helpful = true
+          @vote.save
+          render_json("Thank you for your feedback!")
+        end
       else
-        @vote.helpful = true
-        @vote.save
+        @vote = Vote.create(user: current_user, review: @review, helpful: true)
         render_json("Thank you for your feedback!")
       end
-    else
-      @vote = Vote.create(user: current_user, review: @review, helpful: true)
-      render_json("Thank you for your feedback!")
     end
   end
 
   def downvote
     @review = Review.find(vote_params[:review_id])
     @vote = Vote.find_by(user: current_user, review: @review)
-
-    if @vote
-      if @vote.helpful == false
-        render_json("You can change your answer but you can't answer again.")
+    if current_user == @review.user
+      render_json("You can't vote on your own reviews.")
+    else
+      if @vote
+        if @vote.helpful == false
+          render_json("You can change your answer but you can't answer again.")
+        else
+          @vote.helpful = false
+          @vote.save
+          render_json("Thank you for your feedback!")
+        end
       else
-        @vote.helpful = false
-        @vote.save
+        @vote = Vote.create(user: current_user, review: @review, helpful: false)
         render_json("Thank you for your feedback!")
       end
-    else
-      @vote = Vote.create(user: current_user, review: @review, helpful: false)
-      render_json("Thank you for your feedback!")
     end
   end
 
@@ -42,14 +47,6 @@ class Api::V1::VotesController < Api::V1::ApiController
 
   def vote_params
     params.require(:vote).permit(:review_id)
-  end
-
-  def authorize_user
-    review = Review.find(vote_params[:review_id])
-    if current_user == review.user
-      flash[:alert] = "You can't vote on your own reviews."
-      redirect_to review.movie_theater
-    end
   end
 
   def render_json(message)
