489c46f908f7c64786e9177140cf4c8d913aa676
Greg Rose <gregrose628@gmail.com>
2017-06-08 16:41:22 -0400

    added test for trying to delete theater with admin privileges

--- spec/controllers/movie_theaters_controller_spec.rb
+++ spec/controllers/movie_theaters_controller_spec.rb
@@ -7,4 +7,31 @@ RSpec.describe MovieTheatersController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'DELETE #destroy' do
+    let!(:admin) do
+      create(:user, admin: true)
+    end
+
+    let!(:state) do
+      create(:state)
+    end
+
+    let!(:theater) do
+      create(:movie_theater, state: state)
+    end
+
+    context "success" do
+      it "admin deletes movie theater" do
+        sign_in admin
+        expect{ delete :destroy, params: { id: theater } }.to change(MovieTheater, :count).by(-1)
+      end
+    end
+
+    context "failure" do
+      it "does not delete with lack of admin privileges" do
+        expect { delete :destroy, params: { id: theater } }.to_not change(MovieTheater, :count)
+      end
+    end
+  end
 end
